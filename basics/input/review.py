5f942ca1f2201cb0bd1dc819ae3a0fe35b7401c0
repl.it user <replituser@example.com>
2020-10-17 21:34:21 +0000

    Created a basic input/output program.

---
+++ basics/input/review.py
@@ -0,0 +1,20 @@
+#Calculator
+
+#Gets first number for variable answer
+print("Please enter a number")
+firstnumber = int(input())
+
+#Gets second number for variable answer
+print("Please enter another number")
+secondnumber = int(input())
+
+answer = firstnumber + secondnumber #Adds the inputs together
+
+print("Those two numbers add too {}".format(answer))#Outputs the answer of the two inputs
+
+print("Now please enter a number you liked the multiply this answer by")
+newnumber = int(input())
+
+newanswer = answer * newnumber
+
+print("The final answer is {}".format(newanswer))
