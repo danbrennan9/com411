bd764e367294ba4870b949f9465dd3453a29b20e
repl.it user <replituser@example.com>
2020-10-18 19:35:36 +0000

    Added code to demonstrate the use of multiple nested statement.

--- basics/decisions/nested_decision/nested.py
+++ basics/decisions/nested_decision/nested.py
@@ -1,17 +1,17 @@
-print("What type of cover does the book have?")
+print("What type of cover does the book have?") #gain user input
 type = input()
 
-if (type == "soft"):
+if (type == "soft"): #first check, if true moves to nested if statement, if false moves to elif (next check)
   print("Is the book perfect-bound?")
   perfect_bound = input()
   
-  if (perfect_bound == "yes"):
+  if (perfect_bound == "yes"): #nested if statement
     print("Soft cover, perfect bound books are very popular!")
-  else:
+  else: #nested else statement
       print("Soft covers with coils or stitches are great for short books!")
 
-elif (type == "hard"):
+elif (type == "hard"): #if first IF statement is false, moves to this statement
   print("Books with hard covers can be more expensive!")
 
-else:
+else: #if all checks are false, runs else statement here
       print("Input not understood!")
