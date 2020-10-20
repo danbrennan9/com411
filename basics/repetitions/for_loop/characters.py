24992363da7bff32f546009ffb3ff858de56a488
repl.it user <replituser@example.com>
2020-10-20 15:12:40 +0000

    Added code to demonstrate the use of membership operators with a for loop.

--- basics/repetitions/for_loop/characters.py
+++ basics/repetitions/for_loop/characters.py
@@ -3,7 +3,7 @@ markings = input()
 
 count = 0
 
-for count in range (0, len(markings), 1):
-  print("index", count, ":", markings[count])
+for count in range (0, len(markings), 1): #counts starts on 0, continues through each character of markings by increments of 1
+  print("index", count, ":", markings[count]) #count represents what index, so starts on index 0, and markings count is the loop that incrementally goes up for each character
 
 print("Done!")
--- basics/repetitions/for_loop/reverse.py
+++ basics/repetitions/for_loop/reverse.py
@@ -1,7 +1,7 @@
 print("What phrase do you see?")
 phrase = input()
 
-print("Reversing...")
+print("Reversing...\nThe phrase is ", end="")
 
-for position in range(len(phrase) - 1, -1, -1):
-  print(phrase[position], end="")
+for position in range (len(phrase) - 1, -1, -1): #for loop starts on end character of phrase, loops until it reaches the last character, and goes down in increments of 1
+  print(phrase[position], end="") 
