40ca78f7d084cb40ac27810304f134eca78f59c8
repl.it user <replituser@example.com>
2020-10-20 19:23:01 +0000

    Added code to demonstrate the use of built-in functions:len, ord.

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,15 @@
+
+
+print("Program started!")
+
+print("Please enter a standard character.")
+character = input()
+
+if len(character) == 1:
+ value = ord(character)
+ print("The ASCII code for {} is {}".format(character, value))
+
+else:
+  print("That is an unknown character.")
+
+print("Program ended!")
