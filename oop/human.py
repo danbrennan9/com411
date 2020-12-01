d0b8c21995e5b836d5de72fc9f178031520eca7a
repl.it user <replituser@example.com>
2020-12-01 20:17:46 +0000

    Updated planet class to use a dictionary.

--- oop/human.py
+++ oop/human.py
@@ -4,32 +4,47 @@ class Human: #class
   MAX_ENERGY = 100 #constant class attribute
 
   
-  def __init__(self):
-    self.name = "Human" #attributes with default values
+  def __init__(self, name="Human"):
+    self.name = name #attributes with default values
     self.age = 0
-    self.energy = Human.MAX_ENERGY
+    self.energy = Human.MAX_ENERGY #set the energy to the constant attributre
 
   
   def grow(self): #method that adds 1 to age
     self.age += 1
 
 
-  def eat(self, amount=10):#method that takes an amount, namely 10, and adds it to age
-    self.amount = amount
-    self.age += amount
+  def eat(self, amount):#method that takes an amount and adds it to energy
+    max_energy = self.energy + amount #defining what max energy is
+    
+    if max_energy > Human.MAX_ENERGY: #if these values are above the constant attribute then
+      self.energy = Human.MAX_ENERGY #make self.energy equivilent to attribute
+      return max_energy - self.energy #return the maximum possible energy, minus the self energy value
+    
+    else:
+      self.energy = max_energy
+      return 0
+      
 
 
-  def move(self, distance=10):#same as def eat, but removes distance from age
-    self.distance=distance
-    self.age -= distance
+  def move(self, distance):#same as def eat, but removes distance from energy
+    min_energy = self.energy - distance
+
+    if min_energy < 0: #if min energy is below 0
+      self.energy = 0 #turn it to zero
+      return self.energy - abs(min_energy) #return self energy minus the min energy
+
+    else:
+      self.energy = min_energy
+      return 0
 
   
   def display(self): #function to display text
-    print(f"I am a {self.name}, I am {self.age} years old")
+    print(f"I am a {self.name}, I am {self.age} years old, my energy is {self.energy}")
 
   
   def __repr__(self): #format string representation of object
-    return f'robot(name={self.name}, age={self.age})'
+    return f'human(name={self.name}, age={self.age}, energy={self.energy})'
 
   
   def __str__(self): #informal
@@ -38,4 +53,8 @@ class Human: #class
 
 if (__name__ == "__main__"): 
   human = Human() #create an object
-  human.display() #display it
+  print(repr(human))
+  human.move(10)
+  print(repr(human))
+  human.eat(5)
+  print(repr(human))
---
+++ oop/planet.py
@@ -0,0 +1,36 @@
+from human import Human
+from robot import Robot
+
+class Planet:
+
+  def __init__(self):
+    self.inhabitants = {
+      'humans':[],
+      'robots':[]
+    }
+
+
+  def add_human(self, Human):
+    self.inhabitants['humans'].append(Human)
+
+  def add_robot(self, Robot):
+    self.inhabitants['robots'].append(Robot)
+
+  def remove_human(self, Human):
+    self.inhabitants['humans'].remove(Human)
+
+  def remove_robot(self, Robot):
+    self.inhabitants['robots'].remove(Robot)
+
+  def __repr__(self):
+    return f"planet(humans={self.inhabitants['humans']}, robots={self.inhabitants['robots']})"
+
+  def __str__(self):
+    return f"This planet has {len(self.inhabitants['humans'])} humans and {len(self.inhabitants['robots'])}"
+
+if (__name__ == "__main__"): 
+  planet = Planet()
+  print(repr(planet))
+  print(planet)
+  
+
